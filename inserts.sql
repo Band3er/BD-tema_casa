035850a095d2217db42bea2bcb2a8986804a684b
spyrex <69646109+Band3er@users.noreply.github.com>
2023-12-25 19:12:54 +0200

    added test for every table

---
+++ inserts.sql
@@ -0,0 +1,17 @@
+INSERT INTO TRANSPORTATION VALUES (1, 'masina', 'alaturi de alex', 20);
+INSERT INTO TRANSPORTATION VALUES (2, 'tren', 'plec din gara de la Iasi', 12.2);
+
+INSERT INTO MENU VALUES (1, 'piftie', 'adusa de Maria', 0);
+INSERT INTO MENU VALUES (2, 'carne de pui', 'cumparat de la Kaufland', 11);
+
+INSERT INTO MONEY VALUES (1, 120, 200);
+INSERT INTO MONEY VALUES (2, 50, 455);
+
+INSERT INTO DRINKS VALUES (1, 'vodka', 'de la kaufland', 69.99);
+INSERT INTO DRINKS VALUES (2, 'Coca Cola', 'de la kaufland', 13.99);
+
+INSERT INTO EVENT VALUES (1, 'Padure pe Golgota', '12-MAR-2023', 'IASI');
+INSERT INTO EVENT VALUES (2, 'Cabana la Rege', '28-FEB-2023', 'Gura Humorului');
+
+INSERT INTO PERSONS VALUES (1, 'Sprii', 'Ion', 21, 'B-dul Revolutiei', 0758166384, 'ionut3711@gmail.com', 0, 0, 0, 0);
+INSERT INTO PERSONS VALUES (2, 'Soraba', 'Dia', 19, 'Socola', 0765144089, 'ioan.spiridon@student.tuiasi.ro', 0, 0, 0, 0);
