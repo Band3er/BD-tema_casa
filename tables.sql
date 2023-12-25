be2154a71d287fdcae816a5147bee3fe82566fb8
spyrex <69646109+Band3er@users.noreply.github.com>
2023-12-25 18:11:29 +0200

    added checks

--- tables.sql
+++ tables.sql
@@ -4,8 +4,9 @@ CREATE TABLE EVENT
             CONSTRAINT event_id_event_nn NOT NULL,
         NAME        VARCHAR2(40)
             CONSTRAINT event_name_nn NOT NULL,
+            CONSTRAINT event_name_ck CHECK (REGEXP_LIKE(NAME, '\s+[A-Za-z0-9]+')),
         DATE_EVENT  DATE
-            CONSTRAINT event_date_event_nn NOT NULL,   
+            CONSTRAINT event_date_event_nn NOT NULL,
         LOCATION    VARCHAR2(40)
             CONSTRAINT event_location_nn NOT NULL,
         CONSTRAINT event_id_event_pk PRIMARY KEY (ID_EVENT)
@@ -17,9 +18,11 @@ CREATE TABLE TRANSPORTATION
             CONSTRAINT transportation_id_transport_nn NOT NULL,
         NAME            VARCHAR2(40)
             CONSTRAINT transportation_name_nn NOT NULL,
+            CONSTRAINT transportation_name_ck CHECK (REGEXP_LIKE(NAME, '\s+[A-Za-z0-9]+')),
         DESCRIPTION     VARCHAR2(60),   
         COST            NUMBER(10)
             CONSTRAINT transportation_cost_nn NOT NULL,
+            CONSTRAINT transportation_cost_ck CHECK (COST >= 0)
         CONSTRAINT transportation_id_transport_pk PRIMARY KEY (ID_TRANSPORT)
     );
 
@@ -29,9 +32,11 @@ CREATE TABLE MENU
             CONSTRAINT menu_id_menu_nn NOT NULL,
         NAME            VARCHAR2(40)
             CONSTRAINT menu_name_nn NOT NULL,
+            CONSTRAINT menu_name_ck CHECK (REGEXP_LIKE(NAME, '\s+[A-Za-z0-9]+')),
         DESCRIPTION     VARCHAR2(60),   
         COST            NUMBER(10)
             CONSTRAINT menu_cost_nn NOT NULL,
+            CONSTRAINT menu_cost_ck CHECK (COST >= 0)
         CONSTRAINT menu_id_menu_pk PRIMARY KEY (ID_MENU)
     );
 
@@ -41,6 +46,7 @@ CREATE TABLE MONEY
             CONSTRAINT money_id_money_nn NOT NULL,
         PAYMENT_AMOUNT  NUMBER(12,2),
         TOTAL           NUMBER(12,2),
+            CONSTRAINT money_total_ck CHECK (TOTAL >= 0),
         CONSTRAINT money_id_money_pk PRIMARY KEY (ID_MONEY)
     );
 
@@ -50,9 +56,11 @@ CREATE TABLE DRINKS
             CONSTRAINT drinks_id_drinks_nn NOT NULL,
         NAME            VARCHAR2(40)
             CONSTRAINT drinks_name_nn NOT NULL,
+            CONSTRAINT drinks_name_ck CHECK (REGEXP_LIKE(NAME, '\s+[A-Za-z0-9]+')),
         DESCRIPTION     VARCHAR2(60),   
         COST            NUMBER(10)
             CONSTRAINT drinks_cost_nn NOT NULL,
+            CONSTRAINT drinks_cost_ck CHECK (COST >= 0),
         CONSTRAINT drinks_id_drinks_pk PRIMARY KEY (ID_DRINKS)
     );
 
@@ -62,14 +70,19 @@ CREATE TABLE PERSONS
             CONSTRAINT persons_id_person_nn NOT NULL,
         LAST_NAME       VARCHAR2(30)
             CONSTRAINT persons_last_name_nn NOT NULL,
+            CONSTRAINT persons_last_name_ck CHECK (REGEXP_LIKE(NAME, '[A-Za-z]+\s+')),
         FIRST_NAME      VARCHAR2(30)
             CONSTRAINT persons_first_name_nn NOT NULL,
+            CONSTRAINT persons_first_name_ck CHECK (REGEXP_LIKE(NAME, '[A-Za-z]+\s+')),
         AGE             NUMBER(3)
             CONSTRAINT persons_age_nn NOT NULL,
+            CONSTRAINT persons_age_ck CHECK (AGE > 0),
         ADDRESS         VARCHAR2(100),
         PHONE_NUMBER    NUMBER(10)
             CONSTRAINT persons_phone_number_nn NOT NULL,
+            CONSTRAINT persons_phone_number_ck CHECK (AGE LIKE '07%'),
         EMAIL           VARCHAR2(50),
+            CONSTRAINT persons_email_ck CHECK (EMAIL LIKE '[a-zA-Z0-9._%-]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,8}')
         BUDGET          NUMBER(3),
         TRANSPORT       NUMBER(3),
         MENU            NUMBER(3),
@@ -88,5 +101,3 @@ CREATE TABLE ENTRIES
         CONSTRAINT entries_id_person_fk FOREIGN KEY (ID_PERSON) REFERENCES PERSONS(ID_PERSON),
         CONSTRAINT entries_id_event_fk FOREIGN KEY (ID_EVENT) REFERENCES EVENT(ID_EVENT)
     );
-
-drop table event;
