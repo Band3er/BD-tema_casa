893c1a20ec2ca4cb0ab24454949d735e1451edf0
spyrex <69646109+Band3er@users.noreply.github.com>
2023-12-26 11:17:35 +0200

    2 inserts for all tables

--- inserts.sql
+++ inserts.sql
@@ -2,16 +2,19 @@ INSERT INTO TRANSPORTATION VALUES (1, 'masina', 'alaturi de alex', 20);
 INSERT INTO TRANSPORTATION VALUES (2, 'tren', 'plec din gara de la Iasi', 12.2);
 
 INSERT INTO MENU VALUES (1, 'piftie', 'adusa de Maria', 0);
-INSERT INTO MENU VALUES (2, 'carne de pui', 'cumparat de la Kaufland', 11);
+INSERT INTO MENU VALUES (3, 'carne ', 'cumparat de la Kaufland', 11);
 
 INSERT INTO MONEY VALUES (1, 120, 200);
 INSERT INTO MONEY VALUES (2, 50, 455);
 
 INSERT INTO DRINKS VALUES (1, 'vodka', 'de la kaufland', 69.99);
-INSERT INTO DRINKS VALUES (2, 'Coca Cola', 'de la kaufland', 13.99);
+INSERT INTO DRINKS VALUES (2, 'Coca', 'de la kaufland', 13.99);
 
 INSERT INTO EVENT VALUES (1, 'Padure pe Golgota', '12-MAR-2023', 'IASI');
 INSERT INTO EVENT VALUES (2, 'Cabana la Rege', '28-FEB-2023', 'Gura Humorului');
 
-INSERT INTO PERSONS VALUES (1, 'Sprii', 'Ion', 21, 'B-dul Revolutiei', 0758166384, 'ionut3711@gmail.com', 0, 0, 0, 0);
-INSERT INTO PERSONS VALUES (2, 'Soraba', 'Dia', 19, 'Socola', 0765144089, 'ioan.spiridon@student.tuiasi.ro', 0, 0, 0, 0);
+INSERT INTO PERSONS(id_person, last_name, first_name, age, address, phone_number, email) VALUES (1, 'Sprii', 'Ion', 21, 'B-dul Revolutiei', 758166384, 'ionut3711@gmail.com');
+INSERT INTO PERSONS(id_person, last_name, first_name, age, address, phone_number, email) VALUES (2, 'Soraba', 'Dia', 19, 'Socola', 0765144089, 'ioan.spiridon@student.tuiasi.ro');
+
+INSERT INTO ENTRIES VALUES (1, 1);
+INSERT INTO ENTRIES VALUES (2, 2);
